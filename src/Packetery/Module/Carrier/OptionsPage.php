fed993e2757bc2bdee7343e2858e533db4dde383
Jan Potužník <jan.potuznik@gmail.com>
2022-11-01 13:26:52 +0100

    PES-1051: filter function made static

--- src/Packetery/Module/Carrier/OptionsPage.php
+++ src/Packetery/Module/Carrier/OptionsPage.php
@@ -192,7 +192,7 @@ class OptionsPage {
 			->setRequired( false )
 			->addRule( Form::FLOAT )
 			->addFilter(
-				function ( float $value ) {
+				static function ( float $value ) {
 					return Helper::simplifyFloat( $value, 2 );
 				}
 			)
